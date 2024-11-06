57c9201fe7761874a4e72ab31e2ef192961fc1a2
Achuabhal <22bcais002@caias.in>
2024-11-06 14:33:27 +0530

    Add Sass dependency and create initial styles for home page

--- my-app/src/pages/homepage.tsx
+++ my-app/src/pages/homepage.tsx
@@ -3,17 +3,18 @@ import NavBar from '../assets/components/tsx/NavBar';
 import HeroSection from '../assets/components/tsx/HeroSection';
 import AboutSection from '../assets/components/tsx/AboutSection';
 import Footer from '../assets/components/tsx/Footer';
+import './home.css';
 
 const App: React.FC = () => {
-  return (
-    <>
-      <NavBar />
-      <HeroSection />
-      <AboutSection />
-      <Footer />
-      {/* Additional sections (e.g., services, testimonials, register) can be added here */}
-    </>
-  );
+    return (
+        <div className="appcontainer p-0 p-md-5 pt-md-0">
+            <NavBar />
+            <HeroSection />
+            <AboutSection />
+            <Footer />
+            {/* Additional sections (e.g., services, testimonials, register) can be added here */}
+        </div>
+    );
 };
 
 export default App;
